62a505a51643d45af7718c1fa1bd322c800cbaa1
Christoph Lameter <cl@linux.com>
2011-06-01 12:25:59 -0500

    slub: Not necessary to check for empty slab on load_freelist
    load_freelist is now only branched to only if there are objects available.
    So no need to check the object variable for NULL.
    
    Change-Id: I51525e3acf1ea7c8eda8bc5f317d895820dd8b58
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- mm/slub.c
+++ mm/slub.c
@@ -2012,9 +2012,6 @@ static void *__slab_alloc(struct kmem_cache *s, gfp_t gfpflags, int node,
 			NULL, new.counters,
 			"__slab_alloc"));
 
-load_freelist:
-	VM_BUG_ON(!page->frozen);
-
 	if (unlikely(!object)) {
 		c->page = NULL;
 		stat(s, DEACTIVATE_BYPASS);
@@ -2024,6 +2021,8 @@ load_freelist:
 update_freelist:
 	stat(s, ALLOC_REFILL);
 
+load_freelist:
+	VM_BUG_ON(!page->frozen);
 	c->freelist = get_freepointer(s, object);
 	c->tid = next_tid(c->tid);
 	local_irq_restore(flags);
