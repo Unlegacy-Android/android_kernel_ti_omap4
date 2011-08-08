f4ffb18932e4ac55604059506002b60bbd82ace7
Dandawate Saket <dsaket@ti.com>
2011-08-08 03:05:09 -0700

    TUNA: Add hdmi device to board-tuna-display
    Add hdmi device to board config and setup the mux
    
    Use GPIO 63 as HDMI HPD line.
    
    HDMI vref is made as a supply and requested by the driver.
    On omap4460 the default internal pullups are turned on.
    These are strong pull ups. In tuna we have external pull ups
    So we have to disable the internal pullup to make sure the
    DDC lines are operational.
    
    Change-Id: I6867e94fa9848d74428d1b5410130607ce3654f0
    Signed-off-by: Dandawate Saket <dsaket@ti.com>

--- arch/arm/mach-omap2/board-tuna.c
+++ arch/arm/mach-omap2/board-tuna.c
@@ -599,6 +599,12 @@ static struct regulator_init_data tuna_vcxio = {
 
 };
 
+static struct regulator_consumer_supply tuna_vdac_supply[] = {
+	{
+		.supply = "hdmi_vref",
+	},
+};
+
 static struct regulator_init_data tuna_vdac = {
 	.constraints = {
 		.min_uV			= 1800000,
@@ -608,6 +614,8 @@ static struct regulator_init_data tuna_vdac = {
 		.valid_ops_mask	 = REGULATOR_CHANGE_MODE
 					| REGULATOR_CHANGE_STATUS,
 	},
+	.num_consumer_supplies	= ARRAY_SIZE(tuna_vdac_supply),
+	.consumer_supplies	= tuna_vdac_supply,
 };
 
 static struct regulator_consumer_supply tuna_vusb_supply[] = {
