edbff397c8ef697d70243d7bd0655bd97273860a
Imre Kaloz <kaloz@openwrt.org>
2012-01-26 13:08:57 +0100

    ARM: 7001/2: Wire up support for the XZ decompressor
    Wire up support for the XZ decompressor
    
    Change-Id: If4b9b9fe73617ac6b562ee7012e71fabcaac3dd3
    Signed-off-by: Imre Kaloz <kaloz@openwrt.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

--- arch/arm/boot/compressed/decompress.c
+++ arch/arm/boot/compressed/decompress.c
@@ -47,6 +47,12 @@ extern char * strstr(const char * s1, const char *s2);
 #include "../../../../lib/decompress_unlzma.c"
 #endif
 
+#ifdef CONFIG_KERNEL_XZ
+//#define memmove memmove
+//#define memcpy memcpy
+#include "../../../../lib/decompress_unxz.c"
+#endif
+
 int do_decompress(u8 *input, int len, u8 *output, void (*error)(char *x))
 {
 	return decompress(input, len, NULL, NULL, output, NULL, error);
