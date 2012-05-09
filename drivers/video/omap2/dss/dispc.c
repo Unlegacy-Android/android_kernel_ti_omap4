ca17deda14a632086ee3c32381e1922f672840a2
Lajos Molnar <lajos@ti.com>
2012-05-09 18:00:25 +0530

    OMAPDSS: DISPC: Allow -1 row-pixel increment on OMAP4/5
    DSS used to throw a BUG on pixinc for pixels == 0.  However, this
    is supported on OMAP4/5 where predecimation is enabled, e.g. if
    we are displaying the full input image and
    
    screen_width % x_decim == (x_decim - 1)
    
    Also simplified if-else conditions that in effect calculated the
    same equation.
    
    Change-Id: I44047b677a84a05bdcfb53cfcb8b42f535a560df
    Signed-off-by: Lajos Molnar <lajos@ti.com>
    Signed-off-by: Sreenidhi Koti <sreenidhi@ti.com>
    Signed-off-by: Varadarajan, Charulatha <charu@ti.com>
    FIXUP from B&N OSS 2.2.0: proper check for UV feature
    Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

--- drivers/video/omap2/dss/dispc.c
+++ drivers/video/omap2/dss/dispc.c
@@ -1825,14 +1825,10 @@ static int color_mode_to_bpp(enum omap_color_mode color_mode)
 
 static s32 pixinc(int pixels, u8 ps)
 {
-	if (pixels == 1)
-		return 1;
-	else if (pixels > 1)
-		return 1 + (pixels - 1) * ps;
-	else if (pixels < 0)
-		return 1 - (-pixels + 1) * ps;
-	else
-		BUG();
+	/* DSS with TILER allows 0 pixel rowinc. We have TILER if we have UV */
+	if (!dss_has_feature(FEAT_HANDLE_UV_SEPARATE))
+		BUG_ON(pixels == 0);
+	return 1 + (pixels - 1) * ps;
 }
 
 static void calc_tiler_row_rotation(struct tiler_view_t *view,
