517eebfcf10bd552a86f3b68af9e02183e14b1e8
Andrii Anisov <andrii.anisov@ti.com>
2012-11-26 12:59:10 +0200

    OMAP44xx: i723 fix. Reconfigure ABE DPLL on early kernel init
    Avoid crashes during warm reboot due to DPLL ABE not relocking.
    Currently the errata is documented as effective for OMAP4430 only,
    but the issue is reproducible on 4460 and 4470 as well.
    The fix affects OMAPS00285348 and OMAPS00285422.
    
    Change-Id: I3cdb9ca273bacbc62bf400a70f60572cdf878d78
    Signed-off-by: Andrii Anisov <andrii.anisov@ti.com>

--- arch/arm/mach-omap2/clock44xx_data.c
+++ arch/arm/mach-omap2/clock44xx_data.c
@@ -4090,8 +4090,6 @@ int __init omap4xxx_clk_init(void)
 	if (cpu_is_omap443x()) {
 		cpu_mask = RATE_IN_443X;
 		cpu_clkflg = CK_443X;
-		/* i723 errata fix. Reconfigure DPLL_ABE */
-		omap4_dpll_abe_reconfigure();
 	} else if (cpu_is_omap446x()) {
 		cpu_mask = RATE_IN_446X;
 		cpu_clkflg = CK_446X;
@@ -4099,6 +4097,9 @@ int __init omap4xxx_clk_init(void)
 		cpu_mask = RATE_IN_447X;
 		cpu_clkflg = CK_447X;
 	}
+
+	/* i723 errata fix. Reconfigure DPLL_ABE */
+	omap4_dpll_abe_reconfigure();
 
 	clk_init(&omap2_clk_functions);
 
