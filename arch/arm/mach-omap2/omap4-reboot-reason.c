a24852c16c717920f38a4bdfb1b95ce747b66741
Oleg Drokin <green@linuxhacker.ru>
2013-02-15 01:37:14 -0500

    A hacky way to always reboot from sdcard on crash.
    This loses last_kmsg, unfortunately.

--- arch/arm/mach-omap2/omap4-reboot-reason.c
+++ arch/arm/mach-omap2/omap4-reboot-reason.c
@@ -78,11 +78,22 @@ static struct notifier_block omap_reboot_notifier = {
 static int __init omap_reboot_reason_init(void)
 {
 	void __iomem *sar_base;
+	unsigned char vmmc_val = 0;
 
 	sar_base = omap4_get_sar_ram_base();
 	if (sar_base)
 		strncpy(sar_base + OMAP_REBOOT_REASON_OFFSET,
 			"", OMAP_REBOOT_REASON_SIZE);
+
+	/* A hack, to always start from vmmc on crash */
+	twl_i2c_write_u8(TWL6030_MODULE_ID0, 0x95, 0x9B);
+	do {
+		/*
+		  Wait for the value to be written in the
+		  VMMC_CFG_VOLTAGE register
+		*/
+		twl_i2c_read_u8(TWL6030_MODULE_ID0, &vmmc_val, 0x9B);
+	} while (0x95 != vmmc_val);
 
 	return register_reboot_notifier(&omap_reboot_notifier);
 }
