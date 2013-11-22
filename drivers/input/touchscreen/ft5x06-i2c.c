65a016b78073f7f42d59e188ed2b9eee4d6653b6
David Marble <drmarble1@gmail.com>
2013-11-21 22:58:17 -0600

    Add touch fuzz to improve ovation sensitivity.
    Fuzz has been added to the touch to better register touches.
    The settings are: fuzz_x=3 and fuzz_y=5.
    This makes ovation better register touches as a single touch
    rather than a drag. Motion less than fuzz is registered as a
    single touch. It greatly helps selecting items and page
    turning while reading books.
    
    I tries other values from 2 to 25 and found these to work
    the best.
    
    Change-Id: I237dc9e5854be8045fa3871e4d2a6b46bda7e6bd

--- drivers/input/touchscreen/ft5x06-i2c.c
+++ drivers/input/touchscreen/ft5x06-i2c.c
@@ -123,6 +123,12 @@
 #define FACTORY_MODE3	3
 #define FT5606_TEST_MODE_RX	20
 
+/*
+Add fuzz factor on touch x and y values
+*/
+int fuzz_x = 3,
+    fuzz_y = 5;
+
 enum ftx_factory_mode
 {
 	FACTORY_MODE_0,
@@ -6327,8 +6333,8 @@ static int ftx_input_device_initialize(struct ft5x06 *ts)
 
 	/* Support ST events */
 #if defined(FTX_SUPPORT_ST)
-	input_set_abs_params(input_device, ABS_X,          0, ts->platform_data->maxx, 0, 0);
-	input_set_abs_params(input_device, ABS_Y,          0, ts->platform_data->maxy, 0, 0);
+	input_set_abs_params(input_device, ABS_X,          0, ts->platform_data->maxx, fuzz_x, 0);
+	input_set_abs_params(input_device, ABS_Y,          0, ts->platform_data->maxy, fuzz_y, 0);
 	input_set_abs_params(input_device, ABS_TOOL_WIDTH, 0, FT_LARGE_TOOL_WIDTH,     0, 0);
 	input_set_abs_params(input_device, ABS_PRESSURE,    0, FT_MAXZ,                 0, 0);
 #endif /* defined(FTX_SUPPORT_ST) */
@@ -6362,8 +6368,8 @@ static int ftx_input_device_initialize(struct ft5x06 *ts)
 		}
 
 		/* Set the parameters for multi touch only if we can support MT */
-		input_set_abs_params(input_device, ABS_MT_POSITION_X,  0, ts->platform_data->maxx - 1, 0, 0);
-		input_set_abs_params(input_device, ABS_MT_POSITION_Y,  0, ts->platform_data->maxy - 1, 0, 0);
+		input_set_abs_params(input_device, ABS_MT_POSITION_X,  0, ts->platform_data->maxx - 1, fuzz_x, 0);
+		input_set_abs_params(input_device, ABS_MT_POSITION_Y,  0, ts->platform_data->maxy - 1, fuzz_y, 0);
 		//input_set_abs_params(input_device, ABS_MT_WIDTH_MAJOR, 0, FT_LARGE_TOOL_WIDTH,     0, 0);
 		input_set_abs_params(input_device, ABS_MT_PRESSURE,    0, FT_MAXZ,                 0, 0);
 		input_set_abs_params(input_device, ABS_MT_TOUCH_MAJOR, 0, FT_MAXZ,                 0, 0);
