d4060f3d0fbfd414b90dd5c1bb0646d5ad9f7395
Hashcode <hashcode0f@gmail.com>
2014-06-12 16:51:41 -0700

    mfd: twl6030-gpadc: add conversion by channel function
    Change-Id: Ia74626350b72bcc41a8578d19ace5d90a9e0ceee

--- include/linux/i2c/twl6030-gpadc.h
+++ include/linux/i2c/twl6030-gpadc.h
@@ -148,5 +148,8 @@ struct twl6030_gpadc_user_parms {
 };
 
 int twl6030_gpadc_conversion(struct twl6030_gpadc_request *conv);
+#if defined(CONFIG_MACH_TUNA) || defined(CONFIG_MACH_OMAP4_ESPRESSO)
+int twl6030_get_gpadc_conversion(int channel_no);
+#endif
 
 #endif
