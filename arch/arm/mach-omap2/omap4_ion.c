35dbce12766d14397d071814b387ecc899680616
Kyle Repinski <repinski23@gmail.com>
2015-05-04 02:03:15 -0500

    omap: ion: Remove extra space in debug message.

--- arch/arm/mach-omap2/omap4_ion.c
+++ arch/arm/mach-omap2/omap4_ion.c
@@ -184,7 +184,7 @@ void __init omap_ion_init(void)
 				omap4_ion_heap_tiler_mem_size,
 				omap4_ion_heap_nonsec_tiler_mem_size);
 
-	pr_info(" omap4_smc_addr = 0x%x\n"  \
+	pr_info("omap4_smc_addr = 0x%x\n"  \
 				"omap4_ion_heap_secure_input_addr = 0x%x\n"  \
 				"omap4_ducati_heap_addr = 0x%x\n"  \
 				"omap4_ion_heap_tiler_mem_addr = 0x%x\n"  \
