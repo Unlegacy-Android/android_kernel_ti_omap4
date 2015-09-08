9adf227dd9bec6b57f37819e8b6f3fc82a981281
D. Andrei Măceș <dmaces@nd.edu>
2015-09-07 21:00:43 -0400

    Revert "mm: compaction: fix echo 1 > compact_memory return error issue"
    This reverts commit 63c54425c02a07bfa162189f2839a3f204a37b5b.
    
    Change-Id: I9d5e7b48fd5b455292ce400faf8b13e1b476d3ad

--- mm/compaction.c
+++ mm/compaction.c
@@ -823,12 +823,14 @@ static int compact_node(int nid)
 }
 
 /* Compact all nodes in the system */
-static void compact_nodes(void)
+static int compact_nodes(void)
 {
 	int nid;
 
 	for_each_online_node(nid)
 		compact_node(nid);
+
+	return COMPACT_COMPLETE;
 }
 
 /* The written value is actually unused, all memory is compacted */
@@ -839,7 +841,7 @@ int sysctl_compaction_handler(struct ctl_table *table, int write,
 			void __user *buffer, size_t *length, loff_t *ppos)
 {
 	if (write)
-		compact_nodes();
+		return compact_nodes();
 
 	return 0;
 }
