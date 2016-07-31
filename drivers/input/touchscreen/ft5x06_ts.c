4a61f0093f93b4752f30bacad93d54adc1776fe7
D. Andrei Măceș <dmaces@nd.edu>
2016-07-31 14:32:35 -0400

    input: ft5x06_ts: Add ignore-id-check override (DT/pdata)
    Change-Id: I9bf8f83c8de55b282b01e95007a805d19161930e

--- drivers/input/touchscreen/ft5x06_ts.c
+++ drivers/input/touchscreen/ft5x06_ts.c
@@ -496,6 +496,9 @@ static int ft5x06_parse_dt(struct device *dev,
 	if (pdata->irq_gpio < 0)
 		return pdata->irq_gpio;
 
+	pdata->ignore_id_check = of_property_read_bool(np,
+						"focaltech,ignore-id-check");
+
 	rc = of_property_read_u32(np, "focaltech,family-id", &temp_val);
 	if (!rc)
 		pdata->family_id = temp_val;
@@ -675,7 +678,7 @@ static int ft5x06_ts_probe(struct i2c_client *client,
 		return err;
 	}
 
-	if (pdata->family_id != reg_value) {
+	if ((pdata->family_id != reg_value) && (!pdata->ignore_id_check)) {
 		dev_err(&client->dev, "%s:Unsupported controller\n", __func__);
 		goto free_reset_gpio;
 	}
