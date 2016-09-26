eb4673a6cfc1725359dd67ababac9371c8346eda
D. Andrei Măceș <dmaces@nd.edu>
2016-09-26 16:40:47 -0400

    bq27x00: Also register USB charger support with TWL6030
    Full Palmas header is not needed here, but ifdef charger registration
    notifiers based on selected compatible MFDs.
    
    Change-Id: I79b94a55d32c4b59bec1e54c18eb4cf8602bdee4

--- drivers/power/bq27x00_battery.c
+++ drivers/power/bq27x00_battery.c
@@ -40,7 +40,6 @@
 #include <linux/debugfs.h>
 #include <linux/thermal_framework.h>
 #include <asm/unaligned.h>
-#include <linux/mfd/palmas.h>
 
 #include <linux/power/bq27x00_battery.h>
 
@@ -184,6 +183,14 @@ static enum power_supply_property bq27x00_usb_props[] = {
 module_param(poll_interval, uint, 0644);
 MODULE_PARM_DESC(poll_interval, "battery poll interval in seconds - " \
 				"0 disables polling");
+
+#ifdef CONFIG_PALMAS_USB
+int palmas_usb_register_notifier(struct notifier_block *nb);
+#endif
+#ifdef CONFIG_TWL6030_USB
+int twl6030_usb_register_notifier(struct notifier_block *nb);
+int twl6030_usb_unregister_notifier(struct notifier_block *nb);
+#endif
 
 #ifdef CONFIG_CHARGER_BQ2419x
 bool bq2419x_is_charging_done(void);
@@ -863,6 +870,8 @@ static void bq27x00_powersupply_unregister(struct bq27x00_device_info *di)
 	 */
 	poll_interval = 0;
 
+	twl6030_usb_unregister_notifier(&di->nb);
+
 	cancel_delayed_work_sync(&di->work);
 
 	power_supply_unregister(&di->bat);
@@ -1138,7 +1147,12 @@ static int bq27x00_battery_probe(struct i2c_client *client,
 	i2c_set_clientdata(client, di);
 
 	di->nb.notifier_call = bq27x00_usb_notifier_call;
+#ifdef CONFIG_PALMAS_USB
 	palmas_usb_register_notifier(&di->nb);
+#endif
+#ifdef CONFIG_TWL6030_USB
+	twl6030_usb_register_notifier(&di->nb);
+#endif
 
 	/* Register Battery as cooling device for Case domain */
 	if (di->battery_present) {
