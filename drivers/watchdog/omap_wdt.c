43eadb26fd2d1193fb6186ae913508e42456f123
Andreas Blaesius <skate4life@gmx.de>
2017-06-18 18:19:02 +0200

    Revert "Watchdog: OMAP: Fix the runtime pm code to avoid module getting stuck intransition state."
    This reverts commit 51421e1726a2e5f47d070c23363976b7aafeba4a.
    
    Change-Id: Ic8911f81f2894dbdf1abb35e1bba22a6dffd80b3

--- drivers/watchdog/omap_wdt.c
+++ drivers/watchdog/omap_wdt.c
@@ -134,6 +134,8 @@ static void omap_wdt_set_timeout(struct omap_wdt_dev *wdev)
 	u32 delay_period = GET_WLDR_VAL(timer_margin / 2);
 	void __iomem *base = wdev->base;
 
+	pm_runtime_get_sync(wdev->dev);
+
 	/* just count up at 32 KHz */
 	while (__raw_readl(base + OMAP_WATCHDOG_WPS) & 0x04)
 		cpu_relax();
@@ -146,6 +148,8 @@ static void omap_wdt_set_timeout(struct omap_wdt_dev *wdev)
 	while (__raw_readl(base + OMAP_WATCHDOG_WPS) & 1 << 5)
 		cpu_relax();
 	__raw_writel(delay_period, base + OMAP_WATCHDOG_WDLY);
+
+	pm_runtime_put_sync(wdev->dev);
 }
 
 
@@ -155,9 +159,11 @@ static irqreturn_t omap_wdt_interrupt(int irq, void *dev_id)
 	void __iomem *base = wdev->base;
 	u32 i;
 
+	pm_runtime_get_sync(wdev->dev);
 	omap_wdt_ping(wdev);
 	i = __raw_readl(base + OMAP_WATCHDOG_WIRQSTAT);
 	__raw_writel(i, base + OMAP_WATCHDOG_WIRQSTAT);
+	pm_runtime_put_sync_suspend(wdev->dev);
 	return IRQ_HANDLED;
 }
 
@@ -188,6 +194,7 @@ static int omap_wdt_setup(struct omap_wdt_dev *wdev)
 
 	omap_wdt_enable(wdev);
 
+	pm_runtime_put_sync(wdev->dev);
 	return 0;
 }
 
@@ -217,6 +224,8 @@ static int omap_wdt_release(struct inode *inode, struct file *file)
 	 *      Shut off the timer unless NOWAYOUT is defined.
 	 */
 #ifndef CONFIG_WATCHDOG_NOWAYOUT
+	pm_runtime_get_sync(wdev->dev);
+
 	omap_wdt_disable(wdev);
 
 	/* Disable delay interrupt */
@@ -239,9 +248,11 @@ static ssize_t omap_wdt_write(struct file *file, const char __user *data,
 
 	/* Refresh LOAD_TIME. */
 	if (len) {
+		pm_runtime_get_sync(wdev->dev);
 		spin_lock(&wdt_lock);
 		omap_wdt_ping(wdev);
 		spin_unlock(&wdt_lock);
+		pm_runtime_put_sync(wdev->dev);
 	}
 	return len;
 }
@@ -273,15 +284,18 @@ static long omap_wdt_ioctl(struct file *file, unsigned int cmd,
 			return put_user(omap_prcm_get_reset_sources(),
 					(int __user *)arg);
 	case WDIOC_KEEPALIVE:
+		pm_runtime_get_sync(wdev->dev);
 		spin_lock(&wdt_lock);
 		omap_wdt_ping(wdev);
 		spin_unlock(&wdt_lock);
+		pm_runtime_put_sync(wdev->dev);
 		return 0;
 	case WDIOC_SETTIMEOUT:
 		if (get_user(new_margin, (int __user *)arg))
 			return -EFAULT;
 		omap_wdt_adjust_timeout(new_margin);
 
+		pm_runtime_get_sync(wdev->dev);
 		spin_lock(&wdt_lock);
 		omap_wdt_disable(wdev);
 		omap_wdt_set_timeout(wdev);
@@ -289,6 +303,7 @@ static long omap_wdt_ioctl(struct file *file, unsigned int cmd,
 
 		omap_wdt_ping(wdev);
 		spin_unlock(&wdt_lock);
+		pm_runtime_put_sync(wdev->dev);
 		/* Fall */
 	case WDIOC_GETTIMEOUT:
 		return put_user(timer_margin, (int __user *)arg);
@@ -310,7 +325,9 @@ static int omap_wdt_nb_func(struct notifier_block *nb, unsigned long val,
 	void *v)
 {
 	struct omap_wdt_dev *wdev = container_of(nb, struct omap_wdt_dev, nb);
+	pm_runtime_get_sync(wdev->dev);
 	omap_wdt_ping(wdev);
+	pm_runtime_put_sync_suspend(wdev->dev);
 
 	return NOTIFY_OK;
 }
@@ -429,6 +446,7 @@ static void omap_wdt_shutdown(struct platform_device *pdev)
 	struct omap_wdt_dev *wdev = platform_get_drvdata(pdev);
 
 	if (wdev->omap_wdt_users) {
+		pm_runtime_get_sync(wdev->dev);
 		omap_wdt_ping(wdev);
 		pm_runtime_put_sync(wdev->dev);
 	}
@@ -475,6 +493,7 @@ static int omap_wdt_suspend(struct device *dev)
 	struct omap_wdt_dev *wdev = platform_get_drvdata(pdev);
 
 	if (wdev->omap_wdt_users) {
+		pm_runtime_get_sync(wdev->dev);
 		omap_wdt_disable(wdev);
 		pm_runtime_put_sync_suspend(wdev->dev);
 	}
@@ -491,6 +510,7 @@ static int omap_wdt_resume(struct device *dev)
 		pm_runtime_get_sync(wdev->dev);
 		omap_wdt_enable(wdev);
 		omap_wdt_ping(wdev);
+		pm_runtime_put_sync_suspend(wdev->dev);
 	}
 
 	return 0;
