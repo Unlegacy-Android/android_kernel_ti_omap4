andi34/android_kernel_samsung_espresso

[espresso] Revert "ASoC: wm8994: Ensure there are enough BCLKs for four channels" for espresso
This reverts commit 3b6ae1807d29f8cacd63fddf513110f9308b4c8e.

sound/soc/codecs/wm8994.c
@@ -2761,7 +2761,11 @@ static int wm8994_hw_params(struct snd_pcm_substream *substream,
 		return -EINVAL;
 	}
 
+#ifdef CONFIG_MACH_ESPRESSO
+	bclk_rate = params_rate(params) * 2;
+#else
 	bclk_rate = params_rate(params) * 4;
+#endif
 	switch (params_format(params)) {
 	case SNDRV_PCM_FORMAT_S16_LE:
 		bclk_rate *= 16;

Unlegacy-Android/android_kernel_ti_omap4

Change-Id: I706598aa6dd55582f078f1df701ac8c424828272

sound/soc/codecs/wm8994.c
@@ -2761,7 +2761,11 @@ static int wm8994_hw_params(struct snd_pcm_substream *substream,
 		return -EINVAL;
 	}
 
+#ifdef CONFIG_MACH_OMAP4_ESPRESSO
+	bclk_rate = params_rate(params) * 2;
+#else
 	bclk_rate = params_rate(params) * 4;
+#endif
 	switch (params_format(params)) {
 	case SNDRV_PCM_FORMAT_S16_LE:
 		bclk_rate *= 16;