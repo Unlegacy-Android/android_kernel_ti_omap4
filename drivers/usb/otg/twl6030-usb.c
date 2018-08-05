bitandabit/android_kernel_bn_omap

usb: otg: Add USB_EVENT_NO_CONTACT xceiv event
Also, propagate it in the MUSB/TWL OTG code.
Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

Change-Id: Ia42af327acb327e249e66ba1da39a19b9838ebb2

drivers/usb/otg/twl6030-usb.c
@@ -324,6 +324,8 @@ static irqreturn_t twl6030_usb_irq(int irq, void *_twl)
 			twl->otg.last_event = status;
 		} else {
 			regulator_disable(twl->usb3v3);
+			status = USB_EVENT_NO_CONTACT;
+			twl6030_status = USB_EVENT_NO_CONTACT;
 			goto vbus_notify;
 		}
 		atomic_notifier_call_chain(&twl->otg.notifier,

Unlegacy-Android/android_kernel_ti_omap4

drivers/usb/otg/twl6030-usb.c
@@ -327,6 +327,10 @@ static irqreturn_t twl6030_usb_irq(int irq, void *_twl)
 			twl->otg.last_event = status;
 		} else {
 			regulator_disable(twl->usb3v3);
+#ifdef CONFIG_MACH_OMAP_BN
+			status = USB_EVENT_NO_CONTACT;
+			twl6030_status = status;
+#endif
 			goto vbus_notify;
 		}
 		atomic_notifier_call_chain(&twl->otg.notifier,